61a1976d6efef7eb642e3d11d6acf1490526eee6
Abdurrahman <abdurrahmanpolat453@gmail.com>
2025-10-08 22:19:14 +0300

    👨🏼 chore: reset Package.swift for clean binary distribution

--- Package.swift
+++ Package.swift
@@ -1,29 +1,17 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "APKitDistribution",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11)
+        .iOS(.v15),
+        .macOS(.v12)
     ],
     products: [
         .library(name: "APThemeKit", targets: ["APThemeKit"]),
         .library(name: "APRouterKit", targets: ["APRouterKit"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "APThemeKit",
-            url: "https://github.com/abdurrahmanpolat/APKitDistribution/releases/download/APThemeKit_v1.0.0/APThemeKit.xcframework.zip",
-            checksum: "05393924ba159269f80ac536597fb53367f208d74515976ec3c35c2e9e2a7976"
-        ),
-        .binaryTarget(
-            name: "APRouterKit",
-            url: "https://github.com/abdurrahmanpolat/APKitDistribution/releases/download/APRouterKit_v1.0.0/APRouterKit.xcframework.zip",
-            checksum: "25b8efbaaabd3a2ced230dd538662f0563b1cd12c446a1389e4a263a6e5425d7"
-        ),
-        // Placeholder: CI bu satırı bularak yeni target'ları ekler veya günceller.
-        // DO NOT REMOVE THIS LINE - CI uses it to insert/update binaryTarget blocks.
         // BINARY_TARGETS_PLACEHOLDER
     ]
 )
